c1898ceef7794bb3fddda8185de16e526d6ac8ad
Dev-coconut94 <dev.coconut94@gmail.com>
2020-07-23 16:39:15 +0900

    # 17.3.3회원정보수정및삭제기능구현 (p661)
    -문제1: URI-patern 중복으로 인한 문제가 있었는데,
    원인은 sec02.ex01과 sec02.ex02의 @webservlet이 동일하여 나타난 문제여서
    ex01의 것을 주석하였다.
    (java.lang.IllegalStateException, 위 원인을 컴파일러가 보여줌)
    
    -문제2 : javascript 미숙으로 인해 나오는 문제였다.
    script에서 alert 메세지 기능을 구현해두었는데,
    이 부분에서 window를 windows로 오타로 표기하여 문제가 되었다.
    javascripts를 공부하자.
    
    -문제3 : 어제 공부하다가 input의 value를 name으로 코딩하여서
    출력 텍스트가 원하는 바로 출력되지 않았다.
    
    -mvc 패턴 운용에 대해서 이해는 하였으나
    각 부분에 코딩을 하는것은 아직 미숙하니 게시판을 만들어보면서
    다시정리하자.

--- eclipse-workspace/pro17/src/sec02/ex02/MemberController.java
+++ eclipse-workspace/pro17/src/sec02/ex02/MemberController.java
@@ -65,7 +65,7 @@ public class MemberController extends HttpServlet {
 }
 */
 
-package sec02.ex01;
+package sec02.ex02;
 
 import java.io.IOException;
 import java.util.List;
@@ -80,7 +80,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberController
  */
-/*@WebServlet("/member/*")*/
+@WebServlet("/member/*")
 public class MemberController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	MemberDAO memberDAO;
@@ -93,7 +93,8 @@ public class MemberController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doHandle(request, response);
 	}
 
@@ -101,20 +102,23 @@ public class MemberController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doHandle(request, response);
 	}
 
-	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String nextPage = null;
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html;charset=utf-8");
 		String action = request.getPathInfo();
 		System.out.println("action:" + action);
+
 		if (action == null || action.equals("/listMembers.do")) {
 			List<MemberVO> membersList = memberDAO.listMembers();
 			request.setAttribute("membersList", membersList);
-			nextPage = "/test02/listMembers.jsp";
+			nextPage = "/test03/listMembers.jsp";
 		} else if (action.equals("/addMember.do")) {
 			String id = request.getParameter("id");
 			String pwd = request.getParameter("pwd");
@@ -122,14 +126,35 @@ public class MemberController extends HttpServlet {
 			String email = request.getParameter("email");
 			MemberVO memberVO = new MemberVO(id, pwd, name, email);
 			memberDAO.addMember(memberVO);
+			request.setAttribute("msg", "addMember");
 			nextPage = "/member/listMembers.do";
 
 		} else if (action.equals("/memberForm.do")) {
-			nextPage = "/test02/memberForm.jsp";
+			nextPage = "/test03/memberForm.jsp";
+		} else if (action.equals("/modMemberForm.do")) {
+			String id = request.getParameter("id");
+			MemberVO memInfo = memberDAO.findMember(id);
+			request.setAttribute("memInfo", memInfo);
+			nextPage = "/test03/modMemberForm.jsp";
+		} else if (action.equals("/modMember.do")) {
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+
+			MemberVO memberVO = new MemberVO(id, pwd, name, email);
+			memberDAO.modMember(memberVO);
+			request.setAttribute("msg", "modified");
+			nextPage = "/member/listMembers.do";
+		} else if (action.equals("/delMember.do")) {
+			String id = request.getParameter("id");
+			memberDAO.delMember(id);
+			request.setAttribute("msg", "deleted");
+			nextPage = "/member/listMembers.do";
 		} else {
 			List<MemberVO> membersList = memberDAO.listMembers();
 			request.setAttribute("membersList", membersList);
-			nextPage = "/test02/listMembers.jsp";
+			nextPage = "/test03/listMembers.jsp";
 		}
 		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
 		dispatch.forward(request, response);
