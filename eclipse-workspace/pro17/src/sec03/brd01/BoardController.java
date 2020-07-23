8394e4a237031cca4812a911a83de244fe77a885
Dev-coconut94 <dev.coconut94@gmail.com>
2020-07-23 20:05:29 +0900

    #17.4.1 게시판 글 목록 보기 구현
    문제1 : var="contextPath" 이다 ${}사용해서 오류가 났다.
    
    >>17.4.2 게시판 글쓰기 구현(Create) 기능 진행하자.

---
+++ eclipse-workspace/pro17/src/sec03/brd01/BoardController.java
@@ -0,0 +1,64 @@
+package sec03.brd01;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/board/*")
+public class BoardController extends HttpServlet{
+	BoardService boardService;
+	ArticleVO articleVO;
+	
+	
+	@Override
+	public void init() throws ServletException {
+		boardService = new BoardService();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req,resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doHandle(req,resp);
+	}
+
+	private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nextPage = "";
+		req.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		String action = req.getPathInfo();
+		System.out.println("action : " + action);
+		try {
+			List<ArticleVO> articlesList = new ArrayList<ArticleVO>();
+			if(action == null) {
+				articlesList = boardService.listArticles();
+				req.setAttribute("articlesList", articlesList);
+				nextPage ="/board01/listArticles.jsp";
+			} else if (action.equals("/listArticles.do")) {
+				articlesList = boardService.listArticles();
+				req.setAttribute("articlesList", articlesList);
+				nextPage = "/board01/listArticles.jsp";
+			} else {
+				nextPage = "board01/listArticles.jsp";
+			}
+			
+			RequestDispatcher dispatch = req.getRequestDispatcher(nextPage);
+			dispatch.forward(req, resp);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
